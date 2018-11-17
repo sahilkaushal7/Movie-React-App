2e5103cc13ae0764a75d88393899fe9d2ccbe3f1
Sahil Kaushal <sahilkaushal2013@gmail.com>
2018-11-17 20:35:44 +0530

    Updates

---
+++ precache-manifest.66d886022ef10dae58255733f80d7e4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b44bc6daa219c3c505d7",
+    "url": "/Movie-React-App/static/js/runtime~main.b44bc6da.js"
+  },
+  {
+    "revision": "f1491f1572d14e9117d7",
+    "url": "/Movie-React-App/static/js/main.f1491f15.chunk.js"
+  },
+  {
+    "revision": "d4e417ac1d00d32d9cef",
+    "url": "/Movie-React-App/static/js/1.d4e417ac.chunk.js"
+  },
+  {
+    "revision": "f1491f1572d14e9117d7",
+    "url": "/Movie-React-App/static/css/main.15ba56c3.chunk.css"
+  },
+  {
+    "revision": "12acddb0da70b5cb4cbab68aafb1925d",
+    "url": "/Movie-React-App/index.html"
+  }
+];
